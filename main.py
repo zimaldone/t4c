b5d05afebfee207a1b8e1b339469532c39cf4b10
Simone Ferrigno <simone.ferrigno@vodafone.com>
2018-10-23 21:53:14 +0200

    boh... I try

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-import csv
+import csv, sys, json
+from encoding import encoder_decoder
 
+source_file='hotels.csv';
+destination_json='hotel.json'
 
-with open('hotels.csv', mode="r") as hotels_file:
+with open(source_file, mode='r') as hotels_file:
     csv_reader = csv.reader(hotels_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print ('Column names are').join(row)
-            line_count += 1
-        else:
-            print ('Hotel Details: ', row)
-            line_count += 1
-    print('Processed {line_count} lines.')
+    i = 0
+    columns = '';
+    try:
+        for row in csv_reader:
+            if i == 0:
+                columns=row
+                print ('Column names are', ' '.join(row))
+                i += 1
+            else:
+                # with io.open('data.txt', 'w', encoding='utf-8') as f:
+                # boh=split(row)
+                print ('Hotel Details: ', encoder_decoder.make_unicode(''.join(row)))
+                i += 1
+            print('Processed {line_count} lines.')
+    except csv.Error as e:
+        sys.exit('file %s, line %d: %s' % (source_file, csv_reader.line_num, e))
