dafa5fea2267e2ee0b43961784b357be8dc10a10
Simone Ferrigno <simone.ferrigno@vodafone.com>
2018-10-24 20:32:04 +0200

    test with normal reader

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import csv, sys, json
 from encoding import encoder_decoder
 
-source_file='hotels.csv';
+source_file='hotels.csv'
 destination_json='hotel.json'
 
+row = ''
 with open(source_file, mode='r') as hotels_file:
     csv_reader = csv.reader(hotels_file, delimiter=',')
     i = 0
-    columns = '';
+    columns = ''
     try:
         for row in csv_reader:
             if i == 0:
@@ -17,8 +18,14 @@ with open(source_file, mode='r') as hotels_file:
             else:
                 # with io.open('data.txt', 'w', encoding='utf-8') as f:
                 # boh=split(row)
+                # import pdb; pdb.set_trace()
                 print ('Hotel Details: ', encoder_decoder.make_unicode(''.join(row)))
                 i += 1
-            print('Processed {line_count} lines.')
+                if i > 2:
+                    import pdb; pdb.set_trace()
+                    
+                    break
+
+            print('Processed {} lines.'.format(i))
     except csv.Error as e:
         sys.exit('file %s, line %d: %s' % (source_file, csv_reader.line_num, e))
---
+++ util/json_util.py
@@ -0,0 +1,7 @@
+import json
+
+
+def write_json_to_file(data, file):
+    # logger.info('Writing {}'.format(file))
+    with open(file, 'w') as f:
+        json.dump(data, f, sort_keys=True, indent=4)
